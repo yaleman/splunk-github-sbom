f87e336e75a54bd92b149518ba5cb4a0a23cac6a
James Hodgkinson <james@terminaloutcomes.com>
2023-12-30 10:04:18 +1000

    test tweaks

--- test.sh
+++ test.sh
@@ -3,6 +3,23 @@ if [ -f 'errors.txt' ]; then
 	rm errors.txt
 fi
 
+if [ -z "${GITHUB_TOKEN}" ]; then
+	echo "Please set the GITHUB_TOKEN env var"
+	exit 1
+fi
+if [ -z "${SPLUNK_HEC_HOSTNAME}" ]; then
+	echo "Please set the SPLUNK_HEC_HOSTNAME env var"
+	exit 1
+fi
+if [ -z "${SPLUNK_HEC_TOKEN}" ]; then
+	echo "Please set the SPLUNK_HEC_TOKEN env var"
+	exit 1
+fi
+if [ -z "${SPLUNK_HEC_PORT}" ]; then
+	echo "SPLUNK_HEC_PORT env var unset, defaulting to 8088"
+	SPLUNK_HEC_PORT="8088"
+fi
+
 # find the logs with source=splunk-github-sbom-test
 
 INPUT_GITHUB_TOKEN="${GITHUB_TOKEN}" GITHUB_OUTPUT="./errors.txt" cargo run -- \
